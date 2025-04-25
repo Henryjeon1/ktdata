db14a7439a439a148817e449c82802672db76a3f
HenryJeon1 <heehun.jeon@gmail.com>
2025-04-25 11:34:41 +0900

    generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -286,7 +286,7 @@ WHERE
 
 -- game year
 -- gameid Like '2020%'
-substring(gameid,1,4) >= '2020'
+substring(gameid,1,4) >= '2021'
 and gameid <> '20241001-Suwon-1'
 and ((gameid >='20180401' and gameid <='20181018')
 or (gameid >='20190323' and gameid <='20191001')    
