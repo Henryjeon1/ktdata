14f2410001628ed115cade9413e2062aa3a5c32a
HenryJeon1 <heehun.jeon@gmail.com>
2025-05-10 13:11:49 +0900

    Update KBO_generate_data_par.py

--- KBO_generate_data_par.py
+++ KBO_generate_data_par.py
@@ -286,22 +286,20 @@ WHERE
 
 substring(gameid,1,4) >= '2020'
 and gameid <> '20241001-Suwon-1'
-and (
-		(level = 'KoreaBaseballOrganization' and
-			(
-				(gameid >='20180401' and gameid <='20181018')
-				or (gameid >='20190323' and gameid <='20191001')    
-				or (gameid >='20200504' and gameid <='20201100')
-				or (gameid >='20210402' and gameid <='20211100')
-				or (gameid >='20220401' and gameid <='20221012')
-				or (gameid >='20230401' and gameid <='20231018')
-				or (gameid >='20240323' and gameid <='20241002')
-				or (gameid >='20250321')
-			)
-		)
-		or (level = 'KBO Minors'
+and 
+	(level = 'KoreaBaseballOrganization' and
+		(
+			(gameid >='20180401' and gameid <='20181018')
+			or (gameid >='20190323' and gameid <='20191001')    
+			or (gameid >='20200504' and gameid <='20201100')
+			or (gameid >='20210402' and gameid <='20211100')
+			or (gameid >='20220401' and gameid <='20221012')
+			or (gameid >='20230401' and gameid <='20231018')
+			or (gameid >='20240323' and gameid <='20241002')
+			or (gameid >='20250321')
 		)
 	)
+
 
 order by gameid, pitchno 
 
